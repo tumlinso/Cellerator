e6613b88648bce6f9b5e231a4d0e13291668eedc
Gavin Tumlinson <gavin.tumlinson@tum.de>
2026-08-31 07:25:48 +0000

    Wire geometry and projection value boundaries

--- include/Cellerator/geometry/geometry.hh
+++ include/Cellerator/geometry/geometry.hh
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <Cellerator/execution/projection_value_plane/projection_value_plane_v1.hh>
+#include <Cellerator/geometry/relation_cover.hh>
+#include <Cellerator/geometry/semantic_geometry.hh>
 #include <Cellerator/geometry/optimizer/device/device_assisted_disposition.h>
 #include <Cellerator/geometry/optimizer/portfolio_v1.hh>
 
@@ -19,5 +22,17 @@ struct optimizer_portfolio_readiness_v1 {
 
 optimizer_portfolio_readiness_v1
 validate_integrated_optimizer_portfolio_v1() noexcept;
+
+struct geometry_value_boundary_readiness_v1 {
+    std::uint32_t semantic_geometry_schema = 0;
+    std::uint32_t semantic_cover_schema = 0;
+    std::uint32_t projection_value_schema = 0;
+    bool global_identifiers_are_64_bit = false;
+    bool structure_value_lifetimes_are_separate = false;
+    bool physical_holes_are_non_biological = false;
+};
+
+geometry_value_boundary_readiness_v1
+validate_integrated_geometry_value_boundaries_v1() noexcept;
 
 }  // namespace cellerator::geometry::compiler
