6584f8f99ca7f812fd6df5a1e62acc29d7545310
Gavin Tumlinson <gavin.tumlinson@tum.de>
2026-08-31 06:05:25 +0000

    Adapt v1 relation problems to operation core v2

--- include/Cellerator/compute/operation/operation_core_v2.hh
+++ include/Cellerator/compute/operation/operation_core_v2.hh
@@ -1,5 +1,6 @@
 #pragma once
 
 #include <Cellerator/compute/operation/operation_core_v2/schema.hh>
+#include <Cellerator/compute/operation/operation_core_v2/v1_adapter.hh>
 #include <Cellerator/compute/operation/relation_algebra_v2/composition.hh>
 #include <Cellerator/compute/operation/relation_algebra_v2/relation_algebra.hh>
