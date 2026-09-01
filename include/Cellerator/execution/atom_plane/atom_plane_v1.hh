dcc4662cf12cc4905853fced9575a79246bd923e
Gavin Tumlinson <gavin.tumlinson@tum.de>
2026-09-01 07:09:25 +0000

    Define mutable relation value atom plane

--- include/Cellerator/execution/atom_plane/atom_plane_v1.hh
+++ include/Cellerator/execution/atom_plane/atom_plane_v1.hh
@@ -1,4 +1,5 @@
 #pragma once
 
 #include <Cellerator/execution/atom_plane/external_plane_mapping_v1.hh>
+#include <Cellerator/execution/atom_plane/relation_value_plane_v1.hh>
 #include <Cellerator/execution/atom_plane/structural_plane_binding_v1.hh>
