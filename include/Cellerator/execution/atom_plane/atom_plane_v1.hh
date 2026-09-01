3d3c702c08871b81b61f4108e9d2bc08eb72365c
Gavin Tumlinson <gavin.tumlinson@tum.de>
2026-09-01 07:13:08 +0000

    Define mutable state atom plane

--- include/Cellerator/execution/atom_plane/atom_plane_v1.hh
+++ include/Cellerator/execution/atom_plane/atom_plane_v1.hh
@@ -2,5 +2,6 @@
 
 #include <Cellerator/execution/atom_plane/active_support_overlay_v1.hh>
 #include <Cellerator/execution/atom_plane/external_plane_mapping_v1.hh>
+#include <Cellerator/execution/atom_plane/mutable_state_plane_v1.hh>
 #include <Cellerator/execution/atom_plane/relation_value_plane_v1.hh>
 #include <Cellerator/execution/atom_plane/structural_plane_binding_v1.hh>
