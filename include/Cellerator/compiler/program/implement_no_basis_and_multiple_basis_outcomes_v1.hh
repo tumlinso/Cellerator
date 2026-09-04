5cfb189ffb908ab7f53df1af05b2f3ed3cc3fa79
Gavin Tumlinson <gavin.tumlinson@tum.de>
2026-09-04 11:36:03 +0000

    Implement plural basis outcomes

---
+++ include/Cellerator/compiler/program/implement_no_basis_and_multiple_basis_outcomes_v1.hh
@@ -0,0 +1,3 @@
+#pragma once
+#include <Cellerator/compiler/composition/implement_no_basis_and_multiple_basis_outcomes_v1.hh>
+namespace Cellerator::compiler::program{using composition::basis_selection_v1;using composition::select_basis_outcome_v1;}
