4ceca52366d3100093f9755e54c5dfb01783ceea
Gavin Tumlinson <gavin.tumlinson@tum.de>
2026-09-04 16:08:23 +0000

    Implement biological relation hover

--- src/compiler/tooling/cellerator/tooling_model.hh
+++ src/compiler/tooling/cellerator/tooling_model.hh
@@ -15,4 +15,10 @@ struct completion_item {
 [[nodiscard]] std::vector<completion_item>
 complete_cellerator_syntax(std::string_view source, std::size_t cursor);
 
+struct biological_hover {
+    std::string domain, tag, source_axis, destination_axis, support, orientation;
+    std::string numeric_tuple, mutability, structure_identity, value_generation, source_link;
+};
+[[nodiscard]] biological_hover describe_biological_relation(std::string_view declaration);
+
 }  // namespace cellerator::compiler::tooling::v1
