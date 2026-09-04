877daf39027b874453614c6208fb6de547d8a481
Gavin Tumlinson <gavin.tumlinson@tum.de>
2026-09-04 11:41:45 +0000

    Import cross-operation rewrite search

---
+++ include/Cellerator/compiler/program/import_cross_operation_rewrite_and_fusion_search_v1.hh
@@ -0,0 +1,3 @@
+#pragma once
+#include <Cellerator/compiler/composition/import_cross_operation_rewrite_and_fusion_search_v1.hh>
+namespace Cellerator::compiler::program{using composition::connected_rewrite_v1;using composition::select_connected_rewrites_v1;}
