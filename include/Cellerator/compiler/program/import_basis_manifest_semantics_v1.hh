6247170c355fca547b80ac3fcda017dee73ddefc
Gavin Tumlinson <gavin.tumlinson@tum.de>
2026-09-04 11:34:29 +0000

    Import basis manifest semantics

---
+++ include/Cellerator/compiler/program/import_basis_manifest_semantics_v1.hh
@@ -0,0 +1,3 @@
+#pragma once
+#include <Cellerator/compiler/composition/import_basis_manifest_semantics_v1.hh>
+namespace Cellerator::compiler::program{using composition::basis_manifest_v1;using composition::print_basis_manifest_v1;using composition::parse_basis_manifest_v1;}
