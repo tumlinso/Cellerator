c009b581439d6659b2bd80deaeb967e7ecacc223
Gavin Tumlinson <gavin.tumlinson@tum.de>
2026-09-04 08:47:47 +0000

    Freeze CEIR common artifact compatibility

---
+++ include/Cellerator/compiler/ir/text/ceir_artifact_v1.hh
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <Cellerator/compiler/ir/text/freeze_ceir_common_round_trip_and_artifact_compatibility_v1.hh>
+#include <Cellerator/compiler/ir/text/implement_sectioned_binary_ceir_serialization_v1.hh>
+#include <Cellerator/compiler/ir/text/implement_standalone_ceir_compiler_input_detection_v1.hh>
+#include <Cellerator/compiler/ir/text/implement_validation_mode_plumbing_v1.hh>
