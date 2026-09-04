aeee9a506a3899c2fcaf5946b51c912d3dbc2756
Gavin Tumlinson <gavin.tumlinson@tum.de>
2026-09-04 11:28:36 +0000

    Import multi-parent derivation DAGs

---
+++ include/Cellerator/compiler/program/import_multi_parent_derivation_dags_v1.hh
@@ -0,0 +1,3 @@
+#pragma once
+#include <Cellerator/compiler/composition/import_multi_parent_derivation_dags_v1.hh>
+namespace Cellerator::compiler::program { using composition::derivation_dag_v1; using composition::validate_derivation_dag_v1; }
